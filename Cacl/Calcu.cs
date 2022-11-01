615764743983b7c1aae0fc18cf7b0c66a304720f
Nastya Bilyk <nastyabilyk00@gmail.com>
2022-11-01 14:40:49 +0100

    Calcu commit

---
+++ Cacl/Calcu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cacl
+{
+    public class Calcu
+    {
+        public static double Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Min(int a, int b)
+        {
+            return a - b;
+        }
+        public static double Div(int a, int b)
+        {
+            return a / b;
+        }
+        public static double Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
